10653d86c0c44a230b2e0d4d5086fa79ba5e9be5
amribhatt <amrita2500@gmail.com>
2025-05-23 00:22:07 +0100

    Update hello_world.py

--- my-first-python-project/hello_world.py
+++ my-first-python-project/hello_world.py
@@ -7,3 +7,14 @@ print("""Hello, Github!
       This is my day2
       Hurray!""")
 
+# Calculate the dog's age
+dog_age = 50 / 7
+
+# Create a variable for the integer part of the dog's age
+dog_age_int = int(dog_age)
+
+# Print the formatted string, using the integer age where no decimal is desired
+print(f"""Otto's dog age is {dog_age_int}. So a dog that's about
+{dog_age} would be the same age as Otto. Any dog born about {dog_age}
+years ago would be in the same stage of life as Otto.""")
+
